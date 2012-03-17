8aa9b53d5834a55076f1b1902ee2baf4bddea15a
Andrzej Markiewicz <sowizz@gmail.com>
2012-03-17 19:06:15 +0100

    Dopisano wczytywanie modelu i animacji, ale bez sceny nie ruszy.
    Signed-off-by: Andrzej Markiewicz <sowizz@gmail.com>

---
+++ src/Player/PlayerData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Mogre;
+
+public class PlayerData
+{
+
+    string modelName;
+    string modelFileName;
+
+	public PlayerData()
+	{
+
+	}
+
+    //public void initModel()
+    //{
+    //    modelName = "ninja";
+    //    modelFileName = "ninja.mesh";
+
+    //    SceneManager mSceneMgr = new SceneManager();
+
+    //    Entity ent = mSceneMgr.CreateEntity(modelName, modelFileName);
+
+    //    SceneNode node = mSceneMgr.RootSceneNode.CreateChildSceneNode("NinjaNode");
+
+    //    mSceneMgr.GetEntity(modelName).CastShadows = true;
+    //    mSceneMgr.GetEntity(modelName).Visible = true;
+
+    //    node.AttachObject(ent);
+    //    node.SetDirection(new Mogre.Vector3(0.0f,0.0f,1.0f));
+    //    node.SetScale(0.009f, 0.009f, 0.009f);
+
+    //    AnimationState mAnimState;
+    //    mAnimState = mSceneMgr.GetEntity(modelName).GetAnimationState("Idle");
+    //    mAnimState.Loop = true;
+    //    mAnimState.Enabled = true;
+        
+    //}
+}
